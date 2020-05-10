bd89c9afddf0a4e1c246a703bdb7e27e78ad9a85
v-prusakov <v-prusakov@yandex-team.ru>
2020-05-11 02:35:27 +0300

    adds barnaul

--- src/components/MapSection/index.js
+++ src/components/MapSection/index.js
@@ -27,7 +27,7 @@ class ProgrammSection extends Component {
 				{title: "МК в Абакане", link: "https://vk.com/ps_abakan", geometry: [53.721152, 91.442387]},
 				{title: "МК в Челябинске", link: "https://vk.com/ps_cheliabinsk", geometry: [55.159897, 61.402554]},
 				{title: "МК в Перми", link: "https://vk.com/politsturm_perm", geometry: [58.010450, 56.229434]},
-				{title: "МК в Кургане", link: "https://vk.com/ps_kurgan", geometry: [55.441004, 65.341118]},
+				{title: "МК в Барнауле", link: "https://vk.com/politsturm22", geometry: [53.346785, 83.776856]},
 				{title: "МК в Чите", link: "https://vk.com/marxist_club_chita", geometry: [52.033635, 113.501049]},
 				{title: "МК в Сыктывкаре", link: "https://vk.com/politsturm_syktyvkar", geometry: [61.668793, 50.836399]}
 			]
